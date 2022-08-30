a4ebb68b030b19e81f7776d3a8e3d9ba5f8b2893
Jere Os <50892043+j-3073@users.noreply.github.com>
2022-08-29 21:10:38 -0500

    Se arreglo el formulario de ordenes de admin
    - Se añadio archivo js para administrar la función de ordenes de administrador en front-end.
    - Se corrigieron y eliminaron elementos en adminOrder.ejs
    - Se eliminaron tabulaciones innecesarias para navbar.js

---
+++ src/assets/js/admin/adminOrder.js
@@ -0,0 +1,46 @@
+let buttonsOrder = $('.btnOrder');
+
+const createOrder = (e) => {
+    const itemID = e.target.dataset.id;
+    const formOrder = $('#formOrder'); //Obtienes el div del formulario
+    const productName = $(`.product-name[data-id="${itemID}"]`)[0]; //Obtienes los valores de la tabla
+
+    // Si no existe, añádelo
+    if (!$(`#formOrder .row[data-id="${itemID}"]`).length)
+        formOrder.append(`
+            <div class="row" data-id="${itemID}">
+                <div class="col-4 m-auto py-2">
+                    <div class="form-floating">
+                        <input class="form-control prod-name" id="prod-name-${itemID}"
+                            type="text" placeholder="." value="${productName.textContent}" disabled />
+                        <label for="prod-name-${itemID}">
+                            Nombre
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-3 col-lg-2 m-auto py-2">
+                    <div class="form-floating">
+                        <input class="form-control prod-qty" id="prod-qty-${itemID}"
+                            type="number" placeholder="." value="0" min="0" max="9999" />
+                        <label for="prod-qty-${itemID}">
+                            Cantidad
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-4 col-lg-5 m-auto py-2">
+                    <div class="form-floating">
+                        <textarea class="form-control prod-notes" id="prod-notes-${itemID}"
+                            cols="30" rows="10"></textarea>
+                        <label for="prod-notes-${itemID}">
+                            Notas / Comentarios
+                        </label>
+                    </div>
+                </div>
+            </div>
+        `);
+}
+
+if (buttonsOrder.length)
+    buttonsOrder.on('click', createOrder);
--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,24 +1,28 @@
-          /*///////////////////////BARRA LATERAL///////////////////////*/
-          var sidebar = document.querySelector(".sidebar");
-          var closeBtn = document.querySelector("#btn");
-          var searchBtn = document.querySelector(".bx-search");
+/*///////////////////////BARRA LATERAL///////////////////////*/
+var sidebar = document.querySelector(".sidebar")
+var closeBtn = document.querySelector("#btn")
+var searchBtn = document.querySelector(".bx-search")
 
-          closeBtn.addEventListener("click", ()=>{
-          sidebar.classList.toggle("open");
-          menuBtnChange();//calling the function(optional)
-          });
+if (closeBtn) { // Verifica si las variables selectoras contienen un elemento
+    closeBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("open")
+        menuBtnChange() //calling the function(optional)
+    })
 
-          searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-          sidebar.classList.toggle("open");
-          menuBtnChange(); //calling the function(optional)
-          });
+    // following are the code to change sidebar button(optional)
+    function menuBtnChange() {
+        if (sidebar.classList.contains("open")) {
+            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right") //replacing the iocns class
+        } else {
+            closeBtn.classList.replace("bx-menu-alt-right", "bx-menu") //replacing the iocns class
+        }
+    }
+}
 
-          // following are the code to change sidebar button(optional)
-          function menuBtnChange() {
-          if(sidebar.classList.contains("open")){
-          closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-          }else {
-          closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
-          }
-          }
-          /*///////////////////////BARRA LATERAL///////////////////////*/
+if (searchBtn) // Verifica si las variables selectoras contienen un elemento
+    searchBtn.addEventListener("click", () => {
+        // Sidebar open when you click on the search iocn
+        sidebar.classList.toggle("open")
+        menuBtnChange() //calling the function(optional)
+    })
+/*///////////////////////BARRA LATERAL///////////////////////*/
