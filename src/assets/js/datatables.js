4a6f596e3f99cf71c4aecb719a8e303900d6127d
OrlandoIDGS <orlandoperezlujan@gmail.com>
2022-11-23 19:19:53 -0600

    Export PDF, xls and print table

--- src/assets/js/datatables.js
+++ src/assets/js/datatables.js
@@ -28,7 +28,27 @@ $(document).ready(() => {
             //     { data: 'Usuario' },
             //     { data: 'E-mail' }
             // ]
-            dom: '<"top"f>rt<"bottom"p>',
+            dom: '<"top"f>Brt<"bottom"p>',
+            buttons: [
+                {
+                    extend: 'excelHtml5',
+                    text: '<i class="fas fa-file-excel"></i>',
+                    titleAttr: 'Exportar a Excel',
+                    className: 'btn btn-success'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    text: '<i class="fas fa-file-pdf"></i>',
+                    titleAttr: 'Exportar a PDF',
+                    className: 'btn btn-danger'
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="fa fa-print"></i>',
+                    titleAttr: 'Imprimir',
+                    className: 'btn btn-info'
+                }
+            ],
             responsive: true,
             order: [[0, 'desc']],
             columns: columnOrderable('.data-table'),
--- src/assets/js/datatables_others.js
+++ src/assets/js/datatables_others.js
@@ -16,7 +16,27 @@ $(document).ready(() => {
 
     if ($('.data-table').length) {
         $('.data-table').DataTable({
-            dom: '<"top"f>rt<"bottom"p>',
+            dom: '<"top"f>Brt<"bottom"p>',
+            buttons: [
+                {
+                    extend: 'excelHtml5',
+                    text: '<i class="fas fa-file-excel"></i>',
+                    titleAttr: 'Exportar a Excel',
+                    className: 'btn btn-success'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    text: '<i class="fas fa-file-pdf"></i>',
+                    titleAttr: 'Exportar a PDF',
+                    className: 'btn btn-danger'
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="fa fa-print"></i>',
+                    titleAttr: 'Imprimir',
+                    className: 'btn btn-info'
+                }
+            ],
             responsive: true,
             order: [[0, 'desc']],
             columns: columnOrderable('.data-table'),        
